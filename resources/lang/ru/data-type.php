821574a37f7244c66dc9054f0c4bdb911e1d63f0
Sergey Indenbom <dzorogh@gmail.com>
2022-02-25 12:09:18 +0300

    Percent data-type help text

--- resources/lang/ru/data-type.php
+++ resources/lang/ru/data-type.php
@@ -7,7 +7,7 @@ return [
       AttributeDataType::NUMBER => 'Целое число (10)',
       AttributeDataType::STRING => 'Короткая строка (Это пример)',
       AttributeDataType::RATING => 'Дробное число, рейтинг (2.5)',
-      AttributeDataType::PERCENT => 'Число как процент (0.05 => 5%)',
+      AttributeDataType::PERCENT => 'Дробное число (0.05 = 5%)',
       AttributeDataType::HP => 'Число, л. с. (5)',
       AttributeDataType::MONTHS => 'Число, срок в месяцах (24)',
       AttributeDataType::COUNTRY => 'Код страны в ISO (ru)',
