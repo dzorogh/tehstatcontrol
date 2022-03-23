58e623a62001c7a80569f5474283d9abe0ca0496
Sergey Indenbom <dzorogh@gmail.com>
2022-03-23 09:49:44 +0300

    Cache control

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,10 @@
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Expires" content="0">
+
     </head>
     <body class="min-h-full h-full">
 
