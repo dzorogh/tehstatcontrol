7b6b6b77b1d543274bc7b2b122758f4316ef6216
Sergey Indenbom <dzorogh@gmail.com>
2022-07-05 18:44:08 +0300

    fix: prevent table popup horizontal scroll

--- resources/js/components/AppStatsComment.vue
+++ resources/js/components/AppStatsComment.vue
@@ -4,7 +4,7 @@
       class="p-3 w-12 h-12 cursor-pointer stroke-teal-600"
     />
     <ol
-      class="hidden group-hover:block overflow-auto absolute -inset-x-40 top-0 z-50 p-5 pl-12 max-w-[90vw] list-decimal text-zinc-100 bg-zinc-800 rounded shadow-lg w-100"
+      class="hidden group-hover:block overflow-auto absolute top-0 -right-0 -left-40 z-50 p-5 pl-12 max-w-[90vw] list-decimal text-zinc-100 bg-zinc-800 rounded shadow-lg w-100"
     >
       <li
         v-for="(item, index) in list"
